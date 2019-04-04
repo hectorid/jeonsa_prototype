2bde4ab2e14a3e3eb3d13d5fda10a7b058d2b634
Hector Margittay <hectormargittay@live.com>
2019-04-04 09:35:02 -0300

    Added collision to walls to projectiles

--- scripts/Projectile.gd
+++ scripts/Projectile.gd
@@ -11,3 +11,7 @@ func init(input_pos : Vector2, input_dir : Vector2):
 
 func _physics_process(delta):
  translate(direction * SPEED * delta)
+
+func _on_body_entered(body):
+	if body.is_in_group('wall'):
+		queue_free()
