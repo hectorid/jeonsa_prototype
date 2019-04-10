2d152714fd8a5a16e7d255ed3e5ff92a0a718bbd
hector.margittay <hector.margittay@estagiario.mds.gov.br>
2019-04-10 13:03:30 -0300

    I don't know what i'm doing

--- config/scripts/BaseConfigElement.gd
+++ config/scripts/BaseConfigElement.gd
@@ -6,7 +6,7 @@ var label : String  setget _set_label
 var value setget _set_value
 
 func init(new_label : String, new_value) -> void:
-	_set_label(new_label)
+	_set_label(new_label + ' : ERROR')
 	_set_value(new_value)
 
 func _set_label(new_label : String) -> void:
--- config/scripts/GameConfigMenu.gd
+++ config/scripts/GameConfigMenu.gd
@@ -9,17 +9,20 @@ onready var ConfigList = $CenterContainer/PanelContainer/ScrollContainer/ConfigL
 func _ready():
 	add_config('test', 10)
 	add_config('test', 'batata')
-	add_config('test', 30)
+	add_config('test', 30.5)
 	add_config('test', 40)
 
 func add_config(name : String, value) -> void:
 	var new_element : BaseConfigElement = null
 	
-	match(typeof(value)):
+	match typeof(value):
 		TYPE_INT:
 			new_element = CONFIG_INT_ELEMENT.instance()
 		TYPE_STRING:
 			new_element = CONFIG_STRING_ELEMENT.instance()
+		_:
+			new_element = BASE_CONFIG_ELEMENT.instance()
+		
 	
 	if new_element != null:
 		new_element.init(name, value)
